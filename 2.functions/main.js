308dd120159073f73bee5df679d4899dfe52fb30
max_etf <max_etf@mail.ru>
2022-06-04 01:23:51 +0500

    ready

--- 2.functions/main.js
+++ 2.functions/main.js
@@ -5,3 +5,5 @@ console.log(getArrayParams([1, -4, 7]));
 console.log(makeWork([[10,10,20],[20,19,10]],worker));
 console.log(makeWork([[12,23,31],[41,5,61]],worker));
 console.log(makeWork([[14,21,36],[15,26,39]],worker));
+
+console.log(makeWork([[10, 20, 30], [-40, -50, -65]],worker2));
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,34 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
+  let min = arr [0], max = arr[0], sum = 0, avg;
+  let i = 0;
+  while (i < arr.length) {
+    if (arr[i] > max) max = arr[i];
+    if (arr[i] < min) min = arr[i];
+    sum += arr[i];
+    i++;
+  }
+  avg = (sum / arr.length);
+  avg = Number(avg.toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
+  let sum = 0;
+  arr.forEach(element => {
+    sum += element;
+  });
   // Ваш код
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
+  let max = -Infinity;
+  arrOfArr.forEach(element => {
+    if (func(element) > max) max = func(element);
+  });
   // Ваш кода
   // for ...
   
@@ -27,5 +37,11 @@ function makeWork(arrOfArr, func) {
 
 // Задание 3
 function worker2(arr) {
+  let min = arr[0], max = arr[0];
+  arr.forEach(element => {
+    if (element > max) max = element;
+    if (element < min) min = element;
+  });
+  return Math.abs (max - min)
   // Ваш код
 }
