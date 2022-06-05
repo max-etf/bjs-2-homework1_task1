44b99c6a4661e8368338db4d182e49df1b54b6c4
max_etf <max_etf@mail.ru>
2022-06-05 21:38:40 +0500

    ready

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,14 +1,26 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  let result = true;
 
+arr1.every(function(value,index) {
+  result = result && (arr2[index]===value);
+  return result;
+})
+
+arr2.every(function(value,index) {
+  result = result && (arr1[index]===value);
+  return result;
+})
   // Ваш код
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
+  let resultArr = [];
+  arr.every(function(value) {
+    if ((value > 0)&&(value%3===0)) resultArr.push(value*10);
+    return resultArr;
+  })
   // Ваш код
 
   return resultArr; // array
