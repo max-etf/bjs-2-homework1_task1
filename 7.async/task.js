89ecc07a2a8d57ee03a588fc34c14494cfaf8b06
max_etf <max_etf@mail.ru>
2022-06-14 02:20:01 +0500

    ready

--- 7.async/task.js
+++ 7.async/task.js
@@ -11,12 +11,12 @@ class AlarmClock {
     }
 
      addClock(time, callback, id) {
-        if (id===undefined) throw new Error('error text') ;
+        if (id===undefined) throw new Error('Невозможно идентифицировать будильник. Параметр id не задан') ;
         if (this.alarmCollection.find((object) => {
            /*  ((object.id === id) ? true : false)  -- ээээ ?? а почему не работает тернарник ???*/
            if (object.id === id) return true;
            else return false;
-        })!==undefined) console.error('error') ;
+        })!==undefined) console.error('Будильник с таким id уже существует') ;
         else {
             let alarm = {
                 id,
@@ -49,23 +49,28 @@ class AlarmClock {
      
     getCurrentFormattedTime() {
         let alarmtime = new Date;
-        let alarmtimeString = (alarmtime.getHours() > 10 ? `${alarmtime.getHours()}` : `0${alarmtime.getHours()}`)+":"+(alarmtime.getMinutes() > 10 ? `${alarmtime.getMinutes()}` : `0${alarmtime.getMinutes()}`)
+        let alarmtimeString = (alarmtime.getHours() >= 10 ? `${alarmtime.getHours()}` : `0${alarmtime.getHours()}`)+":"+(alarmtime.getMinutes() >= 10 ? `${alarmtime.getMinutes()}` : `0${alarmtime.getMinutes()}`)
                 return alarmtimeString;
     }
 
     start() {
         function checkClock(alarm) {
             let alarmtime = new Date; 
-            let alarmtimeString = (alarmtime.getHours() > 10 ? `${alarmtime.getHours()}` : `0${alarmtime.getHours()}`)+":"+(alarmtime.getMinutes() > 10 ? `${alarmtime.getMinutes()}` : `0${alarmtime.getMinutes()}`)
+            let alarmtimeString = (alarmtime.getHours() >= 10 ? `${alarmtime.getHours()}` : `0${alarmtime.getHours()}`)+":"+(alarmtime.getMinutes() >= 10 ? `${alarmtime.getMinutes()}` : `0${alarmtime.getMinutes()}`)
             /// а как внутри функиции другого метода обращаться к методу getCurrentFormattedTime ??
            if (alarm.time === alarmtimeString)
            {
                alarm.callback();
+               return;
            }
         }
 
         if (this.timerId === null) {
-            this.timerId = setInterval(this.alarmCollection.forEach(element => {checkClock(element)}),1000)           
+            this.timerId = setInterval(() => {
+                this.alarmCollection.forEach(element => {
+                checkClock(element)
+            })
+        }, 1000)           
         }
     }
 
@@ -77,6 +82,7 @@ class AlarmClock {
     }
 
     printAlarms() {
+        console.log ('total count = '+this.alarmCollection.length)
         this.alarmCollection.forEach(element => {
             console.log(`${element.id}`+"  "+`${element.time}`);
         });
@@ -88,33 +94,58 @@ class AlarmClock {
     } 
 }
 /*task 2*/ 
+function testCase () {
+
 let alarm1 = new AlarmClock;
 
-let ringtone = function() {
-    console.log("get up")
+function ringtone () {
+    console.log("get up1"); 
+    return
 } 
 
-let now = new Date
-let nowString = (now.getHours() > 10 ? `${now.getHours()}` : `0${now.getHours()}`)+":"+(now.getMinutes() > 10 ? `${now.getMinutes()}` : `0${now.getMinutes()}`)
-alarm1.addClock(nowString,f => {ringtone()},1);
+let nowString = alarm1.getCurrentFormattedTime();
+let id = 1;
+alarm1.addClock(nowString,f => {ringtone()},id);
 
 function addMinutes(date, minutes) {
-    var result = new Date(date);
+    let result = new Date(date);
     result.setMinutes(result.getMinutes() + minutes);
     return result;
   }
 
-now = addMinutes(now,1)
-nowString = (now.getHours() > 10 ? `${now.getHours()}` : `0${now.getHours()}`)+":"+(now.getMinutes() > 10 ? `${now.getMinutes()}` : `0${now.getMinutes()}`)
-alarm1.addClock(nowString,f => {ringtone()},2);
+function getFormattedTime(date) {
+    let timeString = (date.getHours() >= 10 ? `${date.getHours()}` : `0${date.getHours()}`)+":"+(date.getMinutes() >= 10 ? `${date.getMinutes()}` : `0${date.getMinutes()}`)
+    return timeString;
+}
 
+/* function ringtone1 (id) {
+        console.log("get up2");
+        alarm1.removeClock(id);    
+}  */
+
+let now = new Date
+now = addMinutes(now,1)
+nowString = getFormattedTime(now);
+id = 2;
+alarm1.addClock(nowString,f => {console.log("get up2");alarm1.removeClock(id);},id);
+
+/* function ringtone2 (id) {
+
+        console.log("get up3");
+        alarm1.clearAlarms();
+        alarm1.printAlarms(); 
+} 
+ */
 
 now = addMinutes(now,1)
-nowString = (now.getHours() > 10 ? `${now.getHours()}` : `0${now.getHours()}`)+":"+(now.getMinutes() > 10 ? `${now.getMinutes()}` : `0${now.getMinutes()}`)
-alarm1.addClock(nowString,f => {ringtone()},3);
+nowString = nowString = getFormattedTime(now);
+alarm1.addClock(nowString,f => {console.log("get up3");alarm1.clearAlarms();alarm1.printAlarms();},3);
 
 //alarm1.removeClock(3);
 
 alarm1.printAlarms();
 
 alarm1.start();
+}
+
+testCase();
